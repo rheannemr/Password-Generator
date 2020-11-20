608a4d11efac81c8bf6058b436f057306c82ae4c
Rheanne Reyes <rheannemr@gmail.com>
2020-11-19 16:01:09 -0800

    Add variables to script

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var enterNumberChar;
+var lowercase;
+var uppercase;
+var number;
+var specialChar;
+
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+specialcharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
 // Write password to the #password input
 function writePassword() {
+
+if (enterNumberChar !== null) {
+  enterNumberChar = parseInt(prompt("How many characters would you like your password to be? Choose between 8 to 128"));
+}
+  lowercase = confirm("Would you like lowercase letters in your password?");
+  
+  uppercase = confirm("Would you like uppercase letters in your password?");
+  
+  number = confirm("Would you like numbers in your password?");
+  
+  specialChar = confirm("Would you like special characters in your password?");
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
